d7c11f9b894fad1b813b59f56c62b3dfb5ad4e15
David Ayrault <dva5236@psu.edu>
2018-03-20 19:44:54 +0000

    Initial Commit

---
+++ content_management.py
@@ -0,0 +1,11 @@
+def Content():
+    APP_CONTENT= {
+        "Home":[["Welcome to My App","/introduction-to-app/"], 
+                ["What are the Issues","/background/"],
+                ["Connect with the Community","/message/"]],
+        "Profile":[["User Profile","/profile/"]],
+        "Messages":[["Messages","/messages/"]],
+        "Settings":[["Terms of Service","/tos/"],
+                   ["Delete Account","/deleteaccount/"]]
+    }
+    return APP_CONTENT
